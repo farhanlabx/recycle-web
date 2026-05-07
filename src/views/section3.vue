f269d0e8ec47db91b38de95a05641e921babfdbc
farhan <you@example.com>
2026-05-07 21:23:18 +0800

    section 2.1 done

--- src/views/section3.vue
+++ src/views/section3.vue
@@ -1,51 +1,63 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <div class="bg-[#25343f] py-20">
-    <div class="max-w-3xl px-20 ml-30">
+  <div class="py-20 flex items-start">
+    <div class="ml-30 max-w-3xl px-20">
       <h2 class="text-white text-4xl font-bold mb-6">
         We’ve been there. <br />
         We know what makes people give up.
       </h2>
       <p class="text-gray-400 text-lg">
-        Recycling information is scattered, outdated, and confusing.
-        People don’t stop because they don’t care. They stop because the process feels frustrating,
-        unclear, and inconvenient.
+        Recycling information is scattered, outdated, and confusing. People
+        don’t stop because they don’t care. They stop because the process feels
+        frustrating, unclear, and inconvenient.
       </p>
     </div>
 
- <div class="max-w-xl ml-24 flex flex-col gap-6 pt-12">
-    <div class="sticky top-16 bg-[#1a1a1a] rounded-3xl p-10 shadow-2xl">
-      <span class="text-3xl inline-block mb-4">🌍</span>
-      <h3 class="text-white text-2xl font-semibold tracking-tight mb-2">Where do I even go?</h3>
-      <p class="text-gray-500 leading-relaxed">Recycling locations are hard to find. Most people just give up when it's not nearby.</p>
+    <div class="ml-24 max-w-xl flex flex-col gap-6">
+      <div class="sticky top-16 bg-green-500 rounded-3xl p-10">
+        <span class="text-3xl mb-4">🌍</span>
+        <h3 class="text-gray-500 text-2xl font-semibold tracking-tight mb-2">
+          Where do I even go?
+        </h3>
+        <p class="text-gray-500">
+          Recycling locations are hard to find. Most people just give up when
+          it's not nearby.
+        </p>
+      </div>
+
+      <div class="sticky top-24 bg-green-600 rounded-3xl p-10">
+        <span class="text-3xl mb-4">♻️</span>
+        <h3 class="text-gray-500 text-2xl font-semibold tracking-tight mb-2">
+          Can this be recycled?
+        </h3>
+        <p class="text-gray-500">
+          Confusing labels make people hesitate. We simplify the rules for every
+          material.
+        </p>
+      </div>
+
+      <div class="sticky top-32 bg-green-700 rounded-3xl p-10">
+        <span class="text-3xl mb-4">⏱️</span>
+        <h3 class="text-gray-500 text-2xl font-semibold tracking-tight mb-2">
+          Too much effort
+        </h3>
+        <p class="text-gray-500">
+          If it’s not easy, it won’t happen. We’re here to cut down the process
+          to seconds.
+        </p>
+      </div>
+
+      <div class="sticky top-40 bg-green-800 rounded-3xl p-10">
+        <span class="text-3xl mb-4">✨</span>
+        <h3 class="text-gray-500 text-2xl font-semibold tracking-tight mb-2">
+          Impact matters
+        </h3>
+        <p class="text-gray-500">
+          Visualize your contribution. Small steps lead to big changes for the
+          planet.
+        </p>
+      </div>
     </div>
-
-    <div class="sticky top-24 bg-[#1a1a1a] rounded-3xl p-10 shadow-2xl">
-      <span class="text-3xl inline-block mb-4">♻️</span>
-      <h3 class="text-white text-2xl font-semibold tracking-tight mb-2">Can this be recycled?</h3>
-      <p class="text-gray-500 leading-relaxed">Confusing labels make people hesitate. We simplify the rules for every material.</p>
-    </div>
-
-    <div class="sticky top-32 bg-[#1a1a1a] rounded-3xl p-10 shadow-2xl">
-      <span class="text-3xl inline-block mb-4">⏱️</span>
-      <h3 class="text-white text-2xl font-semibold tracking-tight mb-2">Too much effort</h3>
-      <p class="text-gray-500 leading-relaxed">If it’s not easy, it won’t happen. We’re here to cut down the process to seconds.</p>
-    </div>
-
-    <div class="sticky top-40 bg-[#1a1a1a] rounded-3xl p-10 shadow-2xl">
-      <span class="text-3xl inline-block mb-4">✨</span>
-      <h3 class="text-white text-2xl font-semibold tracking-tight mb-2">Impact matters</h3>
-      <p class="text-gray-500 leading-relaxed">Visualize your contribution. Small steps lead to big changes for the planet.</p>
-    </div>
-
-    <div class="h-[50vh]"></div>
-
-  </div>
   </div>
 </template>
-
-
-
